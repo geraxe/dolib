60cd0adfc276343fcb10ee3b8db376d44fc51f1b
Yuri Balandin <yuri@balandin.ru>
2021-06-20 16:43:09 +0300

    Fix code coverage

--- tests/test_one_clicks.py
+++ tests/test_one_clicks.py
@@ -12,6 +12,10 @@ def test_crud_one_clicks(client: Client) -> None:
     apps_count = len(apps)
     assert apps_count > 0
 
+    # empty filtered app
+    empty_type_apps = client.one_clicks.filter()
+    assert len(apps) == len(empty_type_apps)
+
     # filter one click app
     filtered_apps = client.one_clicks.filter(app_type="droplet")
     assert isinstance(filtered_apps, list)
@@ -28,6 +32,10 @@ async def test_async_crud_one_clicks(async_client: AsyncClient) -> None:
     apps = await async_client.one_clicks.all()
     apps_count = len(apps)
     assert apps_count > 0
+
+    # empty filtered app
+    empty_type_apps = await async_client.one_clicks.filter()
+    assert len(apps) == len(empty_type_apps)
 
     # filter one click app
     filtered_apps = await async_client.one_clicks.filter(app_type="droplet")
